b901e4c996dbc916667cd6680aa8941c2a00eb29
Samuel König de Oliveira <skoliveira@users.noreply.github.com>
2026-03-23 20:48:21 +0000

    nivel aventureiro

--- tetris.c
+++ tetris.c
@@ -7,7 +7,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de controle de peças.
 // Use as instruções de cada nível para desenvolver o desafio.
 
-#define MAX 5
+#define MAX_FILA 5
+#define MAX_PILHA 3
 
 // --- Estruturas ---
 
@@ -17,20 +18,33 @@ typedef struct {
 } Peca;
 
 typedef struct {
-    Peca pecas[MAX];
+    Peca pecas[MAX_FILA];
     int inicio;
     int fim;
     int total;
 } Fila;
 
+typedef struct {
+    Peca pecas[MAX_PILHA];
+    int topo;
+} Pilha;
+
 // --- Funções ---
 void inicializarFila(Fila *f);
 int filaVazia(Fila *f);
 int filaCheia(Fila *f);
-void enqueue(Fila *f, Peca p);
-void dequeue(Fila *f, Peca *p);
+void enqueue(Fila *f, Peca nova);
+void dequeue(Fila *f, Peca *removida);
 void mostrarFila(Fila *f);
 Peca gerarPeca(int *id);
+
+void inicializarPilha(Pilha *p);
+int pilhaVazia(Pilha *p);
+int pilhaCheia(Pilha *p);
+void push(Pilha *p, Peca nova);
+void pop(Pilha *p, Peca *removida);
+void peek(Pilha *p, Peca *visualizada);
+void mostrarPilha(Pilha *p);
 
 void exibirMenu();
 void limparBufferEntrada();
@@ -49,17 +63,32 @@ int main() {
     //      0 - Sair
     // - A cada remoção, insira uma nova peça ao final da fila.
 
+ 
+    // 🧠 Nível Aventureiro: Adição da Pilha de Reserva
+    //
+    // - Implemente uma pilha linear com capacidade para 3 peças.
+    // - Crie funções como inicializarPilha(), push(), pop(), pilhaCheia(), pilhaVazia().
+    // - Permita enviar uma peça da fila para a pilha (reserva).
+    // - Crie um menu com opção:
+    //      2 - Enviar peça da fila para a reserva (pilha)
+    //      3 - Usar peça da reserva (remover do topo da pilha)
+    // - Exiba a pilha junto com a fila após cada ação com mostrarPilha().
+    // - Mantenha a fila sempre com 5 peças (repondo com gerarPeca()).
+
     // Inicializa a semente com o tempo atual
     srand(time(NULL));
 
     Fila f;
     inicializarFila(&f);
 
+    Pilha p;
+    inicializarPilha(&p);
+
     // id inicial = zero
     int id = 0;
 
     // lotar a fila
-    for(int i=0; i<MAX; i++) {
+    for(int i=0; i<MAX_FILA; i++) {
         enqueue(&f, gerarPeca(&id));
     }
 
@@ -69,16 +98,27 @@ int main() {
         printf("\nEscolha uma opcao: ");
         scanf("%d", &opcao);
         limparBufferEntrada();
+        Peca peca;
         switch (opcao)
         {
         case 1:
-            Peca p;
-            dequeue(&f, &p);
-            mostrarFila(&f);
-            break;
-        case 2:
+            dequeue(&f, &peca);
             enqueue(&f, gerarPeca(&id));
             mostrarFila(&f);
+            mostrarPilha(&p);
+            break;
+        case 2:
+            dequeue(&f, &peca);
+            enqueue(&f, gerarPeca(&id));
+            push(&p, peca);
+            mostrarFila(&f);
+            mostrarPilha(&p);
+            break;
+        case 3:
+            Peca peca;
+            pop(&p, &peca);
+            mostrarFila(&f);
+            mostrarPilha(&p);
             break;
         case 0:
             return 0;
@@ -92,18 +132,6 @@ int main() {
     } while(1);
 
     mostrarFila(&f);
-
-    // 🧠 Nível Aventureiro: Adição da Pilha de Reserva
-    //
-    // - Implemente uma pilha linear com capacidade para 3 peças.
-    // - Crie funções como inicializarPilha(), push(), pop(), pilhaCheia(), pilhaVazia().
-    // - Permita enviar uma peça da fila para a pilha (reserva).
-    // - Crie um menu com opção:
-    //      2 - Enviar peça da fila para a reserva (pilha)
-    //      3 - Usar peça da reserva (remover do topo da pilha)
-    // - Exiba a pilha junto com a fila após cada ação com mostrarPilha().
-    // - Mantenha a fila sempre com 5 peças (repondo com gerarPeca()).
-
 
     // 🔄 Nível Mestre: Integração Estratégica entre Fila e Pilha
     //
@@ -137,35 +165,83 @@ int filaVazia(Fila *f) {
 }
 
 int filaCheia(Fila *f) {
-    return f->total == MAX;
+    return f->total == MAX_FILA;
 }
 
-void enqueue(Fila *f, Peca p) {
+void enqueue(Fila *f, Peca nova) {
     if (filaCheia(f)) {
         printf("Fila cheia. Não é possível inserir.\n");
         return;
     }
  
-    f->pecas[f->fim] = p;
-    f->fim = (f->fim + 1) % MAX;
+    f->pecas[f->fim] = nova;
+    f->fim = (f->fim + 1) % MAX_FILA;
     f->total++;
 }
 
-void dequeue(Fila *f, Peca *p) {
+void dequeue(Fila *f, Peca *removida) {
     if (filaVazia(f)) {
         printf("Fila vazia. Não é possível remover.\n");
         return;
     }
 
-    *p = f->pecas[f->inicio];             
-    f->inicio = (f->inicio + 1) % MAX;     
+    *removida = f->pecas[f->inicio];             
+    f->inicio = (f->inicio + 1) % MAX_FILA;     
     f->total--;                        
 }
 
 void mostrarFila(Fila *f) {
     printf("Fila: ");
-    for (int i = 0, idx = f->inicio; i < f->total; i++, idx = (idx + 1) % MAX) {
-        printf("[Peca: %c, id: %d] ", f->pecas[idx].tipo, f->pecas[idx].id);
+    for (int i = 0, idx = f->inicio; i < f->total; i++, idx = (idx + 1) % MAX_FILA) {
+        printf("[%c %d] ", f->pecas[idx].tipo, f->pecas[idx].id);
+    }
+}
+
+void inicializarPilha(Pilha *p) {
+    p->topo = -1;
+}
+
+int pilhaVazia(Pilha *p) {
+    return p->topo == -1;
+}
+
+int pilhaCheia(Pilha *p) {
+    return p->topo == MAX_PILHA - 1;
+}
+
+void push(Pilha *p, Peca nova) {
+    if (pilhaCheia(p)) {
+        printf("Erro: pilha cheia. Não é possível inserir.\n");
+        return;
+    }
+ 
+    p->topo++;              // Avança o topo
+    p->pecas[p->topo] = nova;  // Insere o novo elemento
+}
+
+void pop(Pilha *p, Peca *removida) {
+    if (pilhaVazia(p)) {
+        printf("Erro: pilha vazia. Não há elementos para remover.\n");
+        return;
+    }
+
+    *removida = p->pecas[p->topo];  // Copia o valor do topo
+    p->topo--;               // Decrementa o topo
+}
+
+void peek(Pilha *p, Peca *visualizada) {
+    if (pilhaVazia(p)) {
+        printf("Pilha vazia. Nada para espiar.\n");
+        return;
+    }
+
+    *visualizada = p->pecas[p->topo];
+}
+
+void mostrarPilha(Pilha *p) {
+    printf("\nPilha (topo -> base): ");
+    for (int i = p->topo; i >= 0; i--) {
+        printf("[%c %d] ", p->pecas[i].tipo, p->pecas[i].id);
     }
     printf("\n\nPressione Enter para continuar...");
     getchar();
@@ -183,7 +259,8 @@ Peca gerarPeca(int *id) {
 
 void exibirMenu() {
     printf("\n1. Jogar peca");
-    printf("\n2. Inserir nova peça");
+    printf("\n2. Reservar Peca");
+    printf("\n3. Usar Peca Reserva");
     printf("\n0. Sair");
 }
 
